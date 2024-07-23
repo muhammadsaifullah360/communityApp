ee075f0eb518d72f0c77da9e3743a9d6eba7fdeb
saif <saif.ullah@devsspace.com>
2024-07-23 11:12:44 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+php version or xammp version is 8.1.25
+composer version 2.5.5
+node version 18.16.0
---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1> Vue 3 with Laravel 10!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
